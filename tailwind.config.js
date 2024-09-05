1757bb95b368fc0c865a799200f0e64beac1c4f9
Yang <crapmeat1@gmail.com>
2024-09-05 15:05:39 +0900

    Hello Wing

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mod: 'jit', //JIT モード活性化
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
